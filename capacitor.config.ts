31266b371ac0522c5c465bb909a7fb679eb8303f
Rajviii <rajvijprajapatirjp24@gmail.com>
2026-08-07 13:33:46 +0530

    Added Capacitor packages

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.key360.syncfusion',
+  appName: 'key360-syncfusion',
+  webDir: 'public'
+};
+
+export default config;
