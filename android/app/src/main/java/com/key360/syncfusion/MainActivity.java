f6985cb815855884fc1fa04a2e48e63fc13c34de
Rajviii <rajvijprajapatirjp24@gmail.com>
2026-08-07 13:55:45 +0530

    Initialize capacitor and add native android platform

---
+++ android/app/src/main/java/com/key360/syncfusion/MainActivity.java
@@ -0,0 +1,5 @@
+package com.key360.syncfusion;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.key360.syncfusion',
   appName: 'key360-syncfusion',
-  webDir: 'public'
+  webDir: 'out'
 };
 
 export default config;
