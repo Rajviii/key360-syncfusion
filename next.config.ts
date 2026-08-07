12bf86ccf1abf59632e078a5dab050907ef83850
Rajviii <rajvijprajapatirjp24@gmail.com>
2026-08-07 13:18:35 +0530

    Modified for build static files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // <-- Crucial: This forces Next.js to build static files
+  images: {
+    unoptimized: true, // Required because Next.js native image optimization needs a server
+  },
 };
 
 export default nextConfig;
